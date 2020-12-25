9349a1943b5a2fca8fbe89a0372dc8cf581e359a
dave.l <dave.l@auxworld.net>
2020-12-25 12:47:04 +0800

    update

--- resources/views/exams/score.blade.php
+++ resources/views/exams/score.blade.php
@@ -102,20 +102,20 @@
                             <tr>
                                 <td>{{ $exam->name }}</td>
                                 <td>{{ implode('/',$exam->units()->pluck('name')->toArray()) }}</td>
-                                <td>{{ $classroom->school->fullName() }}</td>
-                                <td>{{ $classroom->fullName() }}</td>
-                                <td>{{ array_sum($exam->avg_class_score($classroom->id)) }}</td>
+                                <td>{{ $tutor_classroom->school->fullName() }}</td>
+                                <td>{{ $tutor_classroom->fullName() }}</td>
+                                <td>{{ array_sum($exam->avg_class_score($tutor_classroom->id)) }}</td>
                             <!--td>{{ array_sum($exam->avg_score()) }}</td-->
                                 <td>{{ array_sum($exam->total_score()) }}</td>
                                 <td><a href="#" class="target" data-toggle="modal" data-target="#target_modal"
-                                       data-my="{{ json_encode($exam->avg_class_score($classroom->id)) }}"
+                                       data-my="{{ json_encode($exam->avg_class_score($tutor_classroom->id)) }}"
                                        data-total="{{ json_encode($exam->total_score()) }}"
                                        data-avg="{{ json_encode($exam->avg_score()) }}">檢視</a></td>
                                 <td>
-                                    <a href="{{ url('exams/score-detail/'.$exam->id.'/'.$classroom->id) }}">檢視</a>
+                                    <a href="{{ url('exams/score-detail/'.$exam->id.'/'.$tutor_classroom->id) }}">檢視</a>
                                 </td>
                                 <td>
-                                    <a href="{{ url('exams/score-export/'.$exam->id.'/'.$classroom->id) }}"
+                                    <a href="{{ url('exams/score-export/'.$exam->id.'/'.$tutor_classroom->id) }}"
                                        class="btn btn-warning btn-sm">匯出</a>
                                 </td>
                             </tr>
